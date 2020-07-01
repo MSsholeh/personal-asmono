bae36c795e48498d4c83eff8e4d307e562a9ba93
masholeh <mail.masholeh@gmail.com>
2020-07-01 18:53:28 +0700

    update

--- database/seeds/GeneralSettingTableSeeder.php
+++ database/seeds/GeneralSettingTableSeeder.php
@@ -10,13 +10,13 @@ class GeneralSettingTableSeeder extends Seeder
 
         $s = GeneralSetting::create([
             'website_name'     => 'Codenesia',
-            'website_logo' => 'uploads/logoheader.png',
-            'website_favicon' => 'uploads/favicon.png',
+            'website_logo' => 'assets/img/asmono-logo.png',
+            'website_favicon' => 'assets/img/asmono-favicon.png',
             'keyword'    => 'codensia, startup, development, website, aplikasi, yogyakarta',
             'metatext'  => 'Codenesia merupakan salah satu perusahaan yang bergerak di bidang IT, pengembangan website dan pembuatan konten kreatif yang dapat membantu memudahkan pengelolaan bisnis. Didirikan pada tahun 2018 di Yogyakarta, Codenesia didukung dengan semangat muda dan keinginan untuk memberikan dampak positif di bidang IT baik dalam skala nasional maupun internasional. Sadar akan pertumbuhan teknologi yang terus berkembang, Codenesia selalu mengikuti perkembangan teknologi terbaru untuk meningkatkan kualitas perusahaan.',
 
             'about_name'    => 'Asmono Wikan',
-            'about_caption' => 'Journalist',
+            'about_caption' => 'Founder & Chairman MAW Talk',
             'email'         => 'mail@codenesia.id',
             'phone_number'  => '085725629333',
             'address'       => 'Jl. Kaliurang KM.13, Sleman, Yogyakarta',
